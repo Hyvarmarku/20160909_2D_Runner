02575410f444ad4c7478e49ac5ed159d83d2c04d
Markus Hyvärinen <markus.hyvarinen@cs.tamk.fi>
2016-09-16 11:15:01 +0300

    New Spawner

---
+++ Assets/Scripts/DeSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Runner
+{
+    public class DeSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private SpawnerController _spawnnerController;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_spawnnerController == null || !_spawnnerController.AddToPool(other.gameObject))
+                Destroy(other.gameObject);
+
+        }
+    }
+}
+
